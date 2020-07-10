5aefb874a994c5dc5c9cd5c2c3c400123e6b58ee
IgoVeyner <igorveyner95@gmail.com>
2020-07-10 17:43:07 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  
+  attr_reader :sides
+  def initialize(l1, l2, l3)
+    @sides = []
+    @sides.push(l1, l2, l3)
+  end
+
+  def kind
+    sides.each do |s| 
+      raise TriangleError if s <= 0
+    end
+    raise TriangleError if sides[0] + sides[1] <= sides[2] || sides[0] + sides[2] <= sides[1] || sides[1] + sides[2] <= sides[0]
+    return :equilateral if sides[0] == sides[1] && sides[1] == sides[2]
+    return :isosceles if sides[0] == sides[1] || sides[0] == sides[2] || sides[1] == sides[2]
+    return :scalene if sides[0] != sides[1] && sides[0] != sides[2] && sides[1] != sides[2]
+  end
+
+  class TriangleError < StandardError
+  end
 end
+# binding.pry
+# 0
