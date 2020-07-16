1896673b9a412458755c4e55c421962558934f0d
miloscomplex <hicallmesutton@gmail.com>
2020-07-16 10:17:01 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,26 +9,17 @@ class Triangle
 
   attr_accessor :side1, :side2, :side3
 
-  def error
-    # begin
-    #   raise TriangleError
-    # rescue TriangleError => error
-    #   puts error.message
-    # end
-    raise TriangleError
-  end
-
   def kind
     # returns, as a symbol it's type
     #valid types are :equilateral, :isosceles, :scalene
     # binding.pry
 
     if self.side1 + self.side2 < self.side3
-      error
+      raise TriangleError
     elsif self.side1 <= 0 || self.side2 <= 0 || self.side3 <= 0
-      error
+      raise TriangleError
     elsif self.side1 + self.side2 <= self.side3 || self.side2 + self.side3 <= self.side1 || self.side3 + self.side1 <= self.side2
-      error
+      raise TriangleError
     elsif self.side1 == self.side2 && self.side2 == self.side3 && self.side3 == self.side1
       :equilateral
     elsif self.side1 != self.side2 && self.side2 != self.side3 && self.side1 != self.side3
