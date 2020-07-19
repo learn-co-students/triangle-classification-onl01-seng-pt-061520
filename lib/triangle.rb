eb0b8b9b838ea82dcd72e057821e9529262bda28
Cameron Hudson <camnn3@yahoo.com>
2020-07-19 15:57:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
-  # write code here
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
 end
+
+attr_accessor :side1, :side2, :side3
+
+def kind 
+  if self.side1 <= 0 || self.side2 <= 0 || self.side3 <= 0
+    raise TriangleError
+    
+  elsif self.side1 + self.side2 <= self.side3 || self.side2 + self.side3 <= self.side1 || self.side3 + self.side1 <= self.side2
+    raise TriangleError
+    
+    elsif self.side1 == self.side2 && self.side2 != self.side3 && self.side3 == self.side1
+      :equilateral
